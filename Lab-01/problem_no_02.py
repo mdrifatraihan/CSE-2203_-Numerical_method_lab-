5264d02c779def853ce78d14214412565bfadac8
Md Rifat Raihan <mdrifatraihan2632003@gmail.com>
2025-08-15 11:41:47 +0600

    Update problem_no_02.py

--- Lab-01/problem_no_02.py
+++ Lab-01/problem_no_02.py
@@ -1,3 +1,4 @@
+#Reverse a string using a loop
 strng = input("Enter the string : ")
 
 reversed_strng = ""
@@ -6,3 +7,4 @@ for char in strng:
     reversed_strng = char + reversed_strng  
 
 print("Reversed string is : ", reversed_strng)
+
